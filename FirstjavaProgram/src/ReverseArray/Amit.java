8cb4ef924a8054166d0bd28f493a069228d7b8b1
chauhanchandan368-design <chauhanchandan368@gmail.com>
2025-11-10 15:00:47 +0530

    writing this

--- FirstjavaProgram/src/ReverseArray/Amit.java
+++ FirstjavaProgram/src/ReverseArray/Amit.java
@@ -2,6 +2,6 @@ package ReverseArray;
 
 public class Amit {
     public static void main(String[] args){
-        System.out.println("deleting this commit");
+        System.out.println("deleting this commit");//klj
     }
 }
