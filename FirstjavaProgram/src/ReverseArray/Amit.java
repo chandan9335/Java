55dbd656701ed6795ac12b42ef4f5af942c74b36
chauhanchandan368-design <chauhanchandan368@gmail.com>
2025-11-10 14:57:43 +0530

    first commit

--- FirstjavaProgram/src/ReverseArray/Amit.java
+++ FirstjavaProgram/src/ReverseArray/Amit.java
@@ -2,6 +2,6 @@ package ReverseArray;
 
 public class Amit {
     public static void main(String[] args){
-        System.out.println("Hi its my temp commit");
+        System.out.println("deleting this commit");
     }
 }
